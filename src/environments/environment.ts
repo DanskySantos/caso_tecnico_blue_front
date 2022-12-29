2d35208ec534e7c42322093ff49e253399bbf4b4
dansky.mendes <dansky.mendes@postoaki.com.br>
2022-12-29 10:52:51 -0300

    changing to run on netlify

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,4 @@
 export const environment = {
-  production: false,
-  baseUrl: 'http://localhost:8080/api/',
-  AWS_ACCESS_KEY: 'AKIA3MF3YNR5U5YEBM6R',
-  AWS_SECRET_KEY: '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'
+  production: true,
+  baseUrl: 'https://caso-tecnico-backend.herokuapp.com/api/',
 };
